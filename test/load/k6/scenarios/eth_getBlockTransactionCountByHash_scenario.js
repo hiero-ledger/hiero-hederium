145693e0e4fecb5d2ff2b3e6966af7fe657df729
Dosi Kolev <104355829+Dosik13@users.noreply.github.com>
2025-01-28 17:33:10 +0200

    40 implement eth get block transaction count by hash endpoint (#82)
    * feat: add implementation of getBlockTransactionCountByHash
    
    * test: add unit tests for getBlockTransactionCountByHash
    
    * test: add k6 tests for getBlockTransactionCountByHash
    
    * fix: remove block tag from validation
    
    (cherry picked from commit 117a506ace85182dbb53dc0680da18a6cdfef67e)
    Signed-off-by: georgi-l95 <glazarov95@gmail.com>

---
+++ test/load/k6/scenarios/eth_getBlockTransactionCountByHash_scenario.js
@@ -0,0 +1,46 @@
+import { getBlockTransactionCountByHash } from "../scripts/eth_getBlockTransactionCountByHash_test.js";
+
+export const options = {
+  scenarios: {
+    constant_load: {
+      executor: "constant-vus",
+      vus: 10,
+      duration: "30s",
+      gracefulStop: "5s",
+    },
+    stress_test: {
+      executor: "ramping-vus",
+      startVUs: 0,
+      stages: [
+        { duration: "20s", target: 20 },
+        { duration: "30s", target: 20 },
+        { duration: "20s", target: 0 },
+      ],
+      gracefulRampDown: "5s",
+    },
+    spike_test: {
+      executor: "ramping-arrival-rate",
+      startRate: 0,
+      timeUnit: "1s",
+      preAllocatedVUs: 50,
+      maxVUs: 100,
+      stages: [
+        { duration: "10s", target: 10 },
+        { duration: "1m", target: 10 },
+        { duration: "10s", target: 100 },
+        { duration: "3m", target: 100 },
+        { duration: "10s", target: 10 },
+        { duration: "3m", target: 10 },
+        { duration: "10s", target: 0 },
+      ],
+    },
+  },
+  thresholds: {
+    http_req_duration: ["p(95)<500"],
+    http_req_failed: ["rate<0.01"],
+  },
+};
+
+export default function () {
+  getBlockTransactionCountByHash();
+} 
